ee2e0428fdf5633e752595e9639c7fe3ae2babf0
takeru08ma <takeru08ma@gmail.com>
2019-08-29 18:28:04 +0900

    Add setup_pleiades.sh

---
+++ setup_pleiades.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# example.
+#   RubyMine xxx.xxxx.xx
+#   WebStorm xxx.xxxx.xx
+#   Goland 1xxx.xxxx.xx
+#   IDEA-U xxx.xxxx.xx
+#   IDEA-JDK11 xxx.xxxx.xx
+
+# ---------------------------------[functions]----------------------------------
+
+# $1: Application Name
+# $2: Version
+setting_jetbrains_ide() {
+  local version=$2
+  local temp=$1
+  local app_name=${1,,}
+
+  if [ $1 == "RubyMine" -o $1 == "WebStorm" -o $1 == "Goland" ]; then
+    local vmoptions_path="${HOME}/.local/share/JetBrains/Toolbox/apps/${1}/ch-0/${version}.vmoptions"
+    local install_path="$HOME/.local/share/JetBrains/Toolbox/apps/${1}/ch-0/pleiades"
+  elif [ $1 == "IDEA-U" ]; then
+    local vmoptions_path="${HOME}/.local/share/JetBrains/Toolbox/apps/${1}/ch-1/${version}.vmoptions"
+    local install_path="$HOME/.local/share/JetBrains/Toolbox/apps/${1}/ch-1/pleiades"
+  else
+    return
+  fi
+
+  # local install_path="$HOME/.local/share/JetBrains/Toolbox/apps/${1}/ch-0/pleiades"
+  local filename="pleiades.zip"
+
+  if [ ! -e "$HOME/Downloads/$filename" ]; then
+    local uri="http://ftp.jaist.ac.jp/pub/mergedoc/pleiades/build/stable/${filename}"
+    wget -O $HOME/Downloads/$filename $uri
+  fi
+
+  if [ ! -e "$install_path" ]; then
+    unzip $HOME/Downloads/$filename -d $install_path
+  fi
+
+  local pleiades_path="${install_path}/plugins/jp.sourceforge.mergedoc.pleiades/pleiades.jar"
+
+  if [ -e "$pleiades_path" ]; then
+    tee -a $vmoptions_path << EOS > /dev/null
+-Xverify:none
+-javaagent:${pleiades_path}
+-Dsun.java2d.uiScale.enabled=false
+EOS
+  fi
+}
+
+# ---------------------------------[execute]------------------------------------
+
+setting_jetbrains_ide $1 $2
+#!/usr/bin/env bash
